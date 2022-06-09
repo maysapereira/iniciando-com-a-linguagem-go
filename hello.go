79fc06f24d762bfc61efaff31b5a17213fafbbfa
Maysa Pereira <maysalvespereira@gmail.com>
2022-06-08 23:08:26 -0300

    :sparkles: refatora váriaveis utilizando atribuição curta

--- hello.go
+++ hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    var nome string = "Maysa"
-    var versao float32 = 1.1
-    var idade int = 22
+    nome := "Maysa"
+    versao := 1.1
+    idade := 22
     fmt.Println("Olá, eu sou a", nome)
     fmt.Println("Tenho", idade, "anos de idade")
     fmt.Println("Este programa está na versão", versao)
