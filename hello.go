6c262034212d5050a26a799de1265c1fd50824aa
Maysa Pereira <maysalvespereira@gmail.com>
2022-06-08 23:42:46 -0300

    :sparkles: cria condicionais

--- hello.go
+++ hello.go
@@ -8,11 +8,21 @@ func main() {
     fmt.Println("Olá, sra.", nome)
     fmt.Println("Este programa está na versão", versao)
 
-    fmt.Println("1- Iniciar Monitoramento")
-    fmt.Println("2- Exibir Logs")
-    fmt.Println("0- Sair do Programa")
+    fmt.Println("1- Iniciar monitoramento")
+    fmt.Println("2- Exibir logs")
+    fmt.Println("0- Sair do programa")
 
     var comando int
     fmt.Scan(&comando)
     fmt.Println("O comando escolhido foi o", comando)
+
+    if comando == 1 {
+        fmt.Println("Iniciando monitoramento...")
+    } else if comando == 2 {
+        fmt.Println("Exibindo logs...")
+    } else if comando == 0 {
+        fmt.Println("Saindo do programa...")
+    } else {
+        fmt.Println("Escolha uma das opções válidas")
+    }
 }
