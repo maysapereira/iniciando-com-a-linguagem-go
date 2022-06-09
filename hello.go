f9b908387227f9316f64b6da936da826777965b4
Maysa Pereira <maysalvespereira@gmail.com>
2022-06-08 23:04:42 -0300

    :sparkles: cria variavel do tipo int

--- hello.go
+++ hello.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
     var nome string = "Maysa"
     var versao float32 = 1.1
+    var idade int = 22
     fmt.Println("Olá, eu sou a", nome)
+    fmt.Println("Tenho", idade, "anos de idade")
     fmt.Println("Este programa está na versão", versao)
 }
