20ced4ee72d9040715b808abcdad10dd7812d66c
Maysa Pereira <maysalvespereira@gmail.com>
2022-06-08 23:21:56 -0300

    :sparkles: cria função Scanf, que captura o input do usuário e salva em uma variável

--- hello.go
+++ hello.go
@@ -5,8 +5,14 @@ import "fmt"
 func main() {
     nome := "Maysa"
     versao := 1.1
-    idade := 22
-    fmt.Println("Olá, eu sou a", nome)
-    fmt.Println("Tenho", idade, "anos de idade")
+    fmt.Println("Olá, sra.", nome)
     fmt.Println("Este programa está na versão", versao)
+
+    fmt.Println("1- Iniciar Monitoramento")
+    fmt.Println("2- Exibir Logs")
+    fmt.Println("0- Sair do Programa")
+
+    var comando int
+    fmt.Scanf("%d", &comando)
+    fmt.Println("O comando escolhido foi o", comando)
 }
